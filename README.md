eed436962f92b77c989af424c2cd8cb76a414e15
MJ Kubba and Olga Talovikova <pair+mj+olga@allstate.com>
2016-09-07 17:52:22 -0500

    initial creation

---
+++ README.md
@@ -0,0 +1,2 @@
+run the app with
+>node main.js
---
+++ main.js
@@ -0,0 +1,64 @@
+const request =require('request');
+let intervalID;
+
+function getAllRuns() {
+  request.get('http://localhost:9001/api/runs/abc123', (err, res, body) => {
+    console.log(body);
+  });
+}
+
+function startRun() {
+  request.post('http://localhost:9001/api/runs/abc123/start', (err, res, body) => {
+    console.log(res.statusCode);
+    intervalID = setInterval(setPosition, 1000);
+  });
+}
+
+function stopRun() {
+  clearInterval(intervalID);
+
+  request.put('http://localhost:9001/api/runs/abc123/stop', (err, res, body) => {
+    console.log(res.statusCode);
+  });
+}
+
+function random (low, high) {
+  return Math.random() * (high - low) + low;
+}
+
+function setPosition() {
+  const position = {
+      "latitude" : random(-10, 10),
+      "longitude": random(-10, 10),
+      "altitude": random(-1, 1),
+  };
+  var options = {
+    url: 'http://localhost:9001/api/positions/abc123',
+    json: position,
+    method: 'POST'
+  };
+  request(options, (err, res, body) => {
+    if (err) {
+      console.error('Error:', err);
+
+      stopRun();
+
+    } else {
+      console.log('Success:', res.statusCode);
+    }
+  });
+}
+
+startRun();
+
+setTimeout(function() {
+  stopRun();
+}, 30000);
+
+
+// startRun();
+// var intervalID = setInterval(setPosition, 1000);
+
+// getAllRuns();
+//
+// stopRun();
